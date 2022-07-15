17d0700ab7e410e91433004eb2f8fe404a4f28e2
Hector Rodriguez <hector@hrod.dev>
2022-07-15 15:44:08 -0500

    add practice with variable and types

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("Escribe tu nombre:");
+string nombre = Console.ReadLine();
+
+// edad?
+Console.WriteLine("Introduce tu edad:");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+// Ciudad
+Console.WriteLine("Introduce tu ciudad:");
+string ciudad = Console.ReadLine();
+
+Console.Write("Hola, " + nombre);
+Console.Write(". Tienes " + edad);
+Console.Write(". Vives en " + ciudad + ".");
+
+
+
+// Presentar mensaje "Hola,
+/*
+int i = 0;
+long l = 1;
+
+float f = 1.5555f;
+double d = 2.66666666d;
+
+char c = 'c';
+string s = "string";
+
+Console.Write("int: " + i);
+Console.WriteLine(" long: " + l);
+
+Console.Write("float: " + f);
+Console.WriteLine(" double: " + d);
+
+Console.Write("char: " + c);
+Console.WriteLine(" string: " + s);
+*/
