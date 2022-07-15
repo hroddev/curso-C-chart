67a059603b6f2dd9e74421fa43ac748cf0ef9f86
Hector Rodriguez <hector@hrod.dev>
2022-07-15 13:21:22 -0500

    Add project files.

---
+++ ejercicio-01-ingresar-nombre/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Enunciado del ejercicio:
+
+Ejercicio 1
+Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+Ejercicio 2
+Escribe un programa que tome la hora y la escriba por pantalla. */
+
+
+// ejercicio 1
+Console.WriteLine("Escribe tu nombre:");
+string name = Console.ReadLine();
+Console.WriteLine("Su nombre es " + name);
+
+// ejercicio 2
+Console.WriteLine("Ingrese la hora:");
+string hour = Console.ReadLine();
+Console.WriteLine("La hora es " + hour);
